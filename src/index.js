ee0b95790a7152007c7cae6509e0ae9df418c54d
cs44445 <2475546459@qq.com>
2021-12-08 17:54:47 +0800

    完成footer组件的封装

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './assets/css/reset.css'
-import 'antd/dist/antd.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
